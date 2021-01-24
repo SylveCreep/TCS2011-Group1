bf61c397257b56ab0ac437c43b57ac8422aaf174
tamhlt <tamhlt@lqdgroup.net>
2021-01-24 23:07:51 +0700

    change by Matt

---
+++ BackEnd/src/main/java/com/example/server/util/seeder.java
@@ -0,0 +1,16 @@
+/*package com.example.server.util;
+
+import com.example.server.*;
+import com.example.server.dto.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/User/Register", method = RequestMethod.POST)
+public class Seeder {
+    public static User GetRandomUser(){
+
+    }
+}*/
