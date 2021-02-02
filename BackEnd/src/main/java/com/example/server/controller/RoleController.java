966364e88d077344cfce51efed39fc3e4290d02a
tamhlt <tamhlt@lqdgroup.net>
2021-02-02 20:34:01 +0700

    Matt Update 2/2

--- BackEnd/src/main/java/com/example/server/controller/RoleController.java
+++ BackEnd/src/main/java/com/example/server/controller/RoleController.java
@@ -1,11 +1,22 @@
 package com.example.server.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import com.example.server.entity.Role;
+import com.example.server.model.request.CreateRole;
 import com.example.server.service.RoleService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +31,25 @@ public class RoleController {
     @GetMapping("/findRoleByName")
     public Role findRoleByName (@RequestParam("name") String name){
         return roleService.findByName(name);
+    }
+
+    private ResponseEntity<?> getResponseEntity(Object data, String code, String mess, HttpStatus status) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("data",data);
+        response.put("code",code);
+        response.put("messenger",mess);
+        return new ResponseEntity<>(response, status);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping(value = "/create", consumes = {"text/plain", "application/*"}, produces = "application/json")
+    public ResponseEntity<?> createRole(@Valid @RequestBody CreateRole role){
+        try{
+            roleService.saveRole(role);
+            return getResponseEntity("NULL","1","Create new role success", HttpStatus.OK);
+        }
+        catch (Exception e) {
+            return getResponseEntity("NULL","-1","Create new role failed", HttpStatus.BAD_REQUEST);
+        }
     }
 }
---
+++ BackEnd/src/main/java/com/example/server/model/request/CreateRole.java
@@ -0,0 +1,37 @@
+package com.example.server.model.request;
+
+import javax.validation.constraints.Size;
+
+public class CreateRole {
+    
+    @Size(min = 5, max = 5)
+    private String code;
+
+    @Size(max = 255)
+    private String name;
+
+    public String getCode(){
+        return code;
+    }
+
+    public void setCode(String code){
+        this.code = code;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public CreateRole(){
+
+    }
+
+    public CreateRole(@Size(min = 5, max = 5) String code,@Size(max = 255) String name){
+        this.code = code;
+        this.name = name;
+    }
+}
