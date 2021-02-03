66be7446020c09fdc2a92e90e3fdb3eda1891a63
PhungHao <haopvgcs17486@fpt.edu.vn>
2021-02-03 12:09:53 +0700

    fix conflict

--- edunetic-magazine/src/router.js
+++ edunetic-magazine/src/router.js
@@ -3,22 +3,13 @@ import Router from "vue-router";
 
 //import CommentList from "@/pages/comment/CommentList";
 import TheFooter from "@/components/TheFooter";
-import UserList from "@/pages/user/UserList";
+
 import Login from "@/pages/login/Login";
-import RoleList from "@/pages/role/RoleList";
 Vue.use(Router);
 export const router = new Router({
     mode: 'history',
     routes: [
         {
-            name:'Users',
-            path: '/users',
-            component: UserList,
-        }, {
-            name:'Roles',
-            path: '/roles',
-            component: RoleList,
-        }, {
             name:'TheFooter',
             path: '/footers',
             component: TheFooter,
