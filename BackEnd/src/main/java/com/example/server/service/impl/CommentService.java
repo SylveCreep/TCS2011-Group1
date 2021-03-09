24e2ca96303a065181b8ff8356cd200a2fde908d
tamhlt <tamhlt@lqdgroup.net>
2021-03-09 20:20:50 +0700

    Matt /

--- BackEnd/src/main/java/com/example/server/service/impl/CommentService.java
+++ BackEnd/src/main/java/com/example/server/service/impl/CommentService.java
@@ -2,27 +2,46 @@ package com.example.server.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.example.server.dao.CommentDao;
+import com.example.server.dao.ContributionDao;
+import com.example.server.dao.UserDao;
+import com.example.server.dto.CommentDto;
 import com.example.server.entity.Comment;
+import com.example.server.entity.Contribution;
+import com.example.server.entity.User;
+import com.example.server.model.request.CreateComment;
+import com.example.server.service.UserService;
+import com.example.server.util.QueryCheck;
+import com.example.server.util.ResponseUtils;
 
-import org.apache.tomcat.util.http.ResponseUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import antlr.collections.List;
-
 @Service(value = "commentService")
 public class CommentService {
     @Autowired
-    private ResponseUtil responseUtil;
+    private ResponseUtils responseUtil;
 
     @Autowired
     private CommentDao commentDao;
 
     @Autowired
+    private QueryCheck queryCheck;
+
+    @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserDao userDao;
+    
+    @Autowired
+    private ContributionDao contributionDao;
 
     public List<Comment> findAll(){
         List<Comment> list = new ArrayList<>();
@@ -30,5 +49,27 @@ public class CommentService {
         return list;
     }
 
-    public Comment findOne(String )
+    public Comment saveComment(CommentDto comment){
+        Comment nComment = comment.getCommentFromDto();
+        nComment.setContent(comment.getContent());
+        nComment.setContribution(comment.getContribution());
+        nComment.setUser(comment.getUser());
+        nComment.setCode("C" + String.format("%04d", queryCheck.GetHighestId("comment")));
+        return commentDao.save(nComment);
+    }
+
+    public Comment saveComment(CreateComment comment){
+        try {
+            Comment nComment = new Comment();
+            nComment.setCode("C" + String.format("%04d", queryCheck.GetHighestId("comment")));
+            nComment.setContent(comment.getContent());
+            Optional<User> user = userDao.findById(comment.getUserId());
+            nComment.setUser(user.get());
+            Contribution contribution = contributionDao.getOne(comment.getContributionId());
+            nComment.setContribution(contribution);
+            return commentDao.save(nComment);
+        }catch(Exception e){
+            return null;
+        }
+    }
 }
