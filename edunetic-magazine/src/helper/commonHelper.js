6127da04f82e002c0285e828b3275b89ac8ad2b3
sachtan872 <63847087+sachtan872@users.noreply.github.com>
2021-03-10 12:28:26 +0700

    user

--- edunetic-magazine/src/helper/commonHelper.js
+++ edunetic-magazine/src/helper/commonHelper.js
@@ -1,65 +1,68 @@
-import moment from "moment";
+import { DefaultConstants } from "@/constant/DefaultConstant";
+import { UrlConstants } from "@/constant/UrlConstant";
+import axios from "axios";
+
 export const commonHelper = {
-    methods: {
-        calculateAge(dob) {
-            let currentDate = new Date();
-            let birthDate = new Date(dob);
-            let difference = currentDate - birthDate;
-            let age = Math.floor(difference / 31557600000);
-            return age
-        },
-        checkPhone(string) {
-            let numbers = /^[0-9]+$/;
-            if (string.match(numbers)) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        preFormatDate(date) {
-            return moment(String(date)).format(
-                "yyyy-MM-DD HH:mm:ss"
-            );
-
-        },
-        showError(attributes, error) {
-            for (let key of Object.keys(attributes)) {
-                let text = document.querySelector("#" + key);
-                if (Object.prototype.hasOwnProperty.call(error, key)) {
-                    text.style.cssText = "border-color: red";
-                } else {
-                    text.style.cssText = "border-color: #CED4DA";
-                }
-            }
-        },
-        requiredValidate(atributes, objectType) {
-            let list_errors = {};
-            for (let [key, value] of Object.entries(atributes)) {
-                if (!Object.prototype.hasOwnProperty.call(objectType, key) || objectType[key] === "") {
-                    list_errors[key] = value + " field is required";
-                }
-            }
-            return list_errors;
-        },
-        userValidate(atributes, objectType) {
-            let list_errors = {};
-            //validate required attribute
-            list_errors = this.requiredValidate(atributes, objectType);
-
-            //validate age older than 18
-            if (objectType.dateOfBirth) {
-                let age = this.calculateAge(objectType.dateOfBirth) 
-                if (age < 18) {
-                    list_errors.dateOfBirth = "User must be older than 18";
-                }
-            }
-            //validate phone input
-            if (objectType.phoneNumber) {
-                if (!this.checkPhone(objectType.phoneNumber)) { 
-                    this.list_errors.phoneNumber = "Phone number field only can contain 0-9 digits";
-                }
-            }
-            return list_errors;
-        }
+  data() {
+    return {
+      list_faculties: [],
+      list_roles: [],
+      list_errors: [],
+      filter: {
+        column: DefaultConstants.Column, //default column = 'id'
+        sort: DefaultConstants.Sort, //default sort = 'asc'
+        limit: DefaultConstants.Limit, //default limit = 15
+        page: DefaultConstants.Page, //default page = 15
+      }
     }
+  },
+  created() {
+    this.getRoleList();
+    this.getFacultyList();
+  },
+  methods: {
+    getcommonSort($column) {
+      if (this.filter.sort === "asc") {
+        this.filter.sort = "desc";
+      } else if (this.filter.sort === "desc") {
+        this.filter.sort = "asc";
+      }
+      this.filter.column = $column;
+      this.filter
+    },
+    getcommonLimit(limit) {
+      this.filter.limit = limit;
+      this.filter.page = 1;
+      this.filter
+    },
+    changecommonPage(e) {
+      this.filter.page = e;
+      this.filter
+    },
+
+    //USER HELPER FUNCTION
+    getRoleList() {
+      axios
+        .post(UrlConstants.Role + "/filter", this.filter)
+        .then((response) => {
+          this.list_roles = response.data.data;
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+          this.showError(this.errors);
+        });
+    },
+    getFacultyList() {
+      axios
+        .post(UrlConstants.Faculty + "/filter", this.filter)
+        .then((response) => {
+          this.list_faculties = response.data.data;
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+          this.showError(this.errors);
+        });
+    },
+
+  },
 }
