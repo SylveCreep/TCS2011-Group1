986235a1306f76ec6c68074ef3082f0093dfe36e
SylveCreep <63847404+SylveCreep@users.noreply.github.com>
2021-03-23 20:26:24 +0700

    fix contribution dao

--- BackEnd/src/main/java/com/example/server/dao/ContributionDao.java
+++ BackEnd/src/main/java/com/example/server/dao/ContributionDao.java
@@ -5,8 +5,8 @@ import java.util.Date;
 import com.example.server.entity.Contribution;
 import com.example.server.model.response.ContributionResponse;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
