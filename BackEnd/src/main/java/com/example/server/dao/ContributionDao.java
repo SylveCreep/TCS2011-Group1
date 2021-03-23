efa3f95567452bf2e68ed07b22edef128a62e4df
SylveCreep <63847404+SylveCreep@users.noreply.github.com>
2021-03-23 20:22:24 +0700

    fix contribution dao

--- BackEnd/src/main/java/com/example/server/dao/ContributionDao.java
+++ BackEnd/src/main/java/com/example/server/dao/ContributionDao.java
@@ -10,7 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ContributionDao extends JpaRepository<Contribution, Long> {
     
     @Query(value = "SELECT c.* FROM contribution c "+
