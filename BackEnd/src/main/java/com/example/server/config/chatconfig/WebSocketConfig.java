d56265ee9d5ebcfb718c4a2f486bde07651c33e1
SylveCreep <63847404+SylveCreep@users.noreply.github.com>
2021-04-01 20:49:13 +0700

    fix contribution

---
+++ BackEnd/src/main/java/com/example/server/config/chatconfig/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package com.example.server.config.chatconfig;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
+
+    @Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+	}
+
+    @Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		logger.info("register websocket");
+		registry
+			.addEndpoint("/ws")
+			.setAllowedOrigins("*")
+			.withSockJS();
+	}
+}
---
+++ BackEnd/src/main/java/com/example/server/model/response/ChatMessageResponse.java
@@ -0,0 +1,38 @@
+package com.example.server.model.response;
+
+public class ChatMessageResponse {
+    private String content;
+    private String sender;
+    private MessageType messageType;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public MessageType getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(MessageType messageType) {
+        this.messageType = messageType;
+    }
+    
+}
